6dce08f2d260df8cee35fbda8f0972e66f070481
Em Olson <olsonem@seattleu.edu>
2017-07-30 17:50:32 +0000

    configured PHP

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-$API_KEY = 'Put your API key here'; // This will be your Flickr-issued API Key.
-$PAGE_TITLE = 'Kittens and Rainbows'; // This should be a user-friendly page title.
-$SEARCH_TAGS = 'kitten,rainbow'; // This must be a comma-separated list
+$API_KEY = 'ed0fc5cf2c7849e71b698f4e0497a1a1'; // This will be your Flickr-issued API Key.
+$PAGE_TITLE = 'Lakes and Clouds'; // This should be a user-friendly page title.
+$SEARCH_TAGS = 'cloud,lake'; // This must be a comma-separated list
 
 ?>
